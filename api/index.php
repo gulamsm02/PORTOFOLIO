2d9ef200ea40316c824fcb9894f5546c754f09af
Gulam Syahrul Muharom <gulamsyahrulmuharram@gmail.com>
2026-02-21 21:39:54 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
+
+// Pindahkan folder storage ke /tmp agar bisa ditulisi (Writeable)
+foreach (['framework/views', 'framework/cache', 'framework/sessions', 'logs'] as $dir) {
+    if (!is_dir('/tmp/storage/' . $dir)) {
+        mkdir('/tmp/storage/' . $dir, 0755, true);
+    }
+}
+
+// Load aplikasi Laravel
 require __DIR__ . '/../public/index.php';

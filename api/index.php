dfe7260868a83958b11d29ace6ebca4464215947
Gulam Syahrul Muharom <gulamsyahrulmuharram@gmail.com>
2026-02-21 22:19:07 +0700

    ayo

--- api/index.php
+++ api/index.php
@@ -1,7 +1,3 @@
 <?php
-
-// Arahkan storage ke folder temporary Vercel
-putenv('APP_STORAGE=/tmp/storage');
-
-// Panggil file index Laravel yang asli di folder public
-require __DIR__ . '/../public/index.php';
+// Tes apakah PHP jalan
+echo "PHP Jalan di Vercel! APP_KEY adalah: " . getenv('APP_KEY');

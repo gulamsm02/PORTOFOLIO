08ab2678f5ea688b8e07b3e6d787c00af36a0e7c
Gulam Syahrul Muharom <gulamsyahrulmuharram@gmail.com>
2026-02-22 10:26:12 +0700

    Fix: Hard upload assets to bypass gitignore cache

--- api/index.php
+++ api/index.php
@@ -7,16 +7,15 @@ foreach (['/views', '/cache', '/sessions'] as $path) {
     }
 }
 
-// FORCE OVERRIDE - Ini yang paling penting
+// FORCE OVERRIDE
 putenv("SESSION_DRIVER=cookie");
 putenv("LOG_CHANNEL=stderr");
 putenv("DB_CONNECTION=sqlite");
 putenv("DB_DATABASE=:memory:");
 putenv("VIEW_COMPILED_PATH=$tmpDir/views");
 
-// ... kode mkdir yang lama ...
-
-putenv("ASSET_URL=" . $_ENV['ASSET_URL']); // Paksa ambil dari env Vercel
-putenv("APP_CONFIG_CACHE=/tmp/config.php"); // Hindari cache lama
+// GUNAKAN "/" AGAR RELATIF KE FOLDER PUBLIC
+putenv("ASSET_URL=/"); 
+putenv("APP_URL=https://portofolio-a453vj7s0-gulam-syahrul-muharoms-projects.vercel.app");
 
 require __DIR__ . '/../public/index.php';
